1d96973416281c75f6501017aaa19a5f76be6b60
Adil Busra Muhammad <muhammadadil@students.usu.ac.id>
2026-05-22 01:19:55 +0700

    Fix: bikin h2 untuk database sementara

--- src/main/java/com/camprent/medan/MedanApplication.java
+++ src/main/java/com/camprent/medan/MedanApplication.java
@@ -1,5 +1,7 @@
 package com.camprent.medan;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +11,4 @@ public class MedanApplication {
     public static void main(String[] args) {
         SpringApplication.run(MedanApplication.class, args);
     }
-
 }
