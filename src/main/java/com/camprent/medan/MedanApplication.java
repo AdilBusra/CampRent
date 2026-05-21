07ce9b4fcd967a70dd701e803afa6fe98ab152dd
Adil Busra Muhammad <muhammadadil@students.usu.ac.id>
2026-05-21 20:32:36 +0700

    Initial commit

---
+++ src/main/java/com/camprent/medan/MedanApplication.java
@@ -0,0 +1,13 @@
+package com.camprent.medan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/camprent/medan/MedanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camprent.medan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
